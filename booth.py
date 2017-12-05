94ef4205f43083241473b4cd92d37b73d8e9c556
Brandon Rice <brandonium21@gmail.com>
2017-12-04 21:15:26 -0600

    Added dependencies

--- booth.py
+++ booth.py
@@ -3,6 +3,7 @@ import picamera
 import Image
 import uuid
 from random import randint
+import facebook
 
 #initialize
 
@@ -17,62 +18,65 @@ cam.resolution = (800,1200)
 cam.brightness = 60
 
 text = input("prompt")
-if t_photo:
-	#Take Photo
-	cam.start_preview()
+try:
+	if t_photo:
+		#Take Photo
+		cam.start_preview()
 
-	# show overlay on preview
-	# Load the arbitrarily sized image
-	skin = "skin-{}.jpg".format(randint(0, 2))
-	img = Image.open(skin)
-	pad = Image.new('RGB', (
-    	((img.size[0] + 31) // 32) * 32,
-    	((img.size[1] + 15) // 16) * 16,
-    ))
-	pad.paste(img, (0, 0))
-	o = cam.add_overlay(pad.tobytes(), size=img.size)
-	o.alpha = 128
-	o.layer = 3
+		# show overlay on preview
+		# Load the arbitrarily sized image
+		skin = "skin-{}.jpg".format(randint(0, 2))
+		img = Image.open(skin)
+		pad = Image.new('RGB', (
+	    	((img.size[0] + 31) // 32) * 32,
+	    	((img.size[1] + 15) // 16) * 16,
+	    ))
+		pad.paste(img, (0, 0))
+		o = cam.add_overlay(pad.tobytes(), size=img.size)
+		o.alpha = 128
+		o.layer = 3
 
-	#count Down
-	cam.annotate_text = 'Get Ready!'
-	sleep(1)
-	cam.annotate_text = 'Uno'
-	sleep(1)
-	cam.annotate_text = '2'
-	sleep(1)
-	cam.annotate_text = 'Three'
-	sleep(1)
+		#count Down
+		cam.annotate_text = 'Get Ready!'
+		sleep(1)
+		cam.annotate_text = 'Uno'
+		sleep(1)
+		cam.annotate_text = '2'
+		sleep(1)
+		cam.annotate_text = 'Three'
+		sleep(1)
 
-	#Name and capture photo
-	current_image = 'booth_cap_{}.jpg'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
-	cam.capture(current_image)
+		#Name and capture photo
+		current_image = 'booth_cap_{}.jpg'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S"))
+		cam.capture(current_image)
 
-	#turn of camera
-	cam.stop_preview()
+		#turn of camera
+		cam.stop_preview()
 
-	#Overlay Photo
-	background = Image.open(current_image)
-	overlay = Image.open(skin)
+		#Overlay Photo
+		background = Image.open(current_image)
+		overlay = Image.open(skin)
 
-	background = background.convert("RGBA")
-	overlay = overlay.convert("RGBA")
+		background = background.convert("RGBA")
+		overlay = overlay.convert("RGBA")
 
-	new_img = Image.blend(background, overlay, 0.5)
-	#Save Photo
-	final_photo = "booth-{}.png".format(str(uuid.uuid4()))
-	new_img.save(final_photo ,"PNG")
+		new_img = Image.blend(background, overlay, 0.5)
+		#Save Photo
+		final_photo = "booth-{}.png".format(str(uuid.uuid4()))
+		new_img.save(final_photo ,"PNG")
 
 
-	#Upload Photo
-	graph = facebook.GraphAPI(oauth_access_token)
-	photo = open(final_photo, "rb")
-	graph.put_object("me", "photos", message="From Brandon's Photo booth", source=photo.read())
-	photo.close()
+		#Upload Photo
+		graph = facebook.GraphAPI(oauth_access_token)
+		photo = open(final_photo, "rb")
+		graph.put_object("me", "photos", message="From Brandon's Photo booth", source=photo.read())
+		photo.close()
 
-	#delete photos
-	os.remove(final_photo)
-	os.remove(current_image)
+		#delete photos
+		os.remove(final_photo)
+		os.remove(current_image)
 
-	# Reset
-	t_photo = False
+		# Reset
+		t_photo = False
+except KeyboardInterrupt:
+	print('Booth Stopped!')
